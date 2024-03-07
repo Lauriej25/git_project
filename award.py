bbe4be7d2ed41ddca12d98271016066f623f701a
Lawrance Joseph <lwdj91@email.com>
2024-03-07 09:32:09 +0000

    Added clear comments for clatiy

--- award.py
+++ award.py
@@ -5,6 +5,15 @@ print()
 
 # Function to get valid time input from user
 def get_valid_time(prompt):
+        """
+    Prompt the user to enter a positive integer for the time.
+    
+    Args:
+    - prompt (str): The prompt message to display.
+    
+    Returns:
+    - int: The positive integer entered by the user.
+    """
     while True:
         try:
             time = int(input(prompt))
@@ -14,16 +23,17 @@ def get_valid_time(prompt):
         except ValueError:
             print("Invalid input! Please enter a positive integer.")
 
+# Get the finishing times for each event from the user
 running = get_valid_time("Enter your finished running time in minutes: ")
 swimming = get_valid_time("Enter your finished swimming time in minutes: ")
 cycling = get_valid_time("Enter your finished cycling time in minutes: ")
 
-# Print the total time by adding all three times together
+# Calculate the total time to complete the triathlon
 qual_time = running + swimming + cycling
 print("Total time to complete triathlon:", qual_time, "minutes")
 print()
 
-# Set the qualifying criteria and print the type of award to be presented
+# Determine the award based on the qualifying time
 if qual_time < 101 and qual_time > 0:  # Award time will be equal to and between 0mins & 100mins
     print("You have been awarded Provincial Colours")
 elif qual_time < 106 and qual_time > 100:  # Award time will be equal to and between 101mins & 105mins
@@ -32,4 +42,3 @@ elif 106 <= qual_time <= 110:  # Award time will be equal to and between 105mins
     print("You have been awarded Provincial Scroll")
 else:
     print("You receive no award")
-    
